e9880b6e492120f71f4bd08b9299338d56b0c97b
Sami <sam445element@gmail.com>
2020-03-14 22:15:36 +0200

    projectiles

--- src/Game/player.py
+++ src/Game/player.py
@@ -1,9 +1,16 @@
 import math
 import pygame
+import projectile
 #from main import arenaHeight, arenaWidth
 
 class Player:
-
+    gotHit=False
+    cooldown=0
+    cooldownMax=100
+    def getHitstate(self):
+        return self.gotHit
+    def setHit(self):
+        self.gotHit=True
     def setImage(self,img):
         self.img=img
     def __init__(self,agent,startX,startY,img):
@@ -29,14 +36,19 @@ class Player:
         self.angle = (self.angle - 0.05 )%(2*math.pi)# % 2.0 - 1
         #self.playerY -= 0.6*math.cos(self.angle)
 
-
+    def fire(self,ent,canShoot):
+        if (canShoot):
+            ent.append(projectile.Projectile(self.playerX+16+64*math.cos(self.angle),self.playerY+16+64*math.sin(self.angle),self.angle))
+            self.cooldown=self.cooldownMax
     def rotateRight(self):
         self.angle = (self.angle + 0.05 )%(2*math.pi)# % 2.0 - 1
         #print(self.angle)
     
     
     def getAction(self):
-        self.agent.getAction(self,self)
+        self.agent.getAction(self,self,self.cooldown<=0)
+        if(self.cooldown>0):
+            self.cooldown=self.cooldown-1
 
     def getHitBox(self):
         return [(self.playerX , self.playerY), (self.playerX, self.playerY+64), (self.playerX + 64, self.playerY +64), (self.playerX+64, self.playerY)]
